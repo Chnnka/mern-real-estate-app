476f4b0816888f2186c1c13c026405cdbc07fd21
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-01-14 19:51:02 +0530

    header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaSearch } from 'react-icons';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -10,6 +12,32 @@ const Header = () => {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
+        <form
+          action=""
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
+          <input
+            type="text"
+            placeholder="Search ..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/signIn">
+            <li className="text-slate-700 hover:underline">Sign In</li>
+          </Link>
+        </ul>
       </div>
     </header>
   );
